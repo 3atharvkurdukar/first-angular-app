485a12041d72c26fc4dd857bcf75678ec93bb137
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-05-31 01:06:22 +0530

    Using component templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // styles: ['h1 { color: red; }'],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template: '<app-server></app-server><app-server></app-server>'
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
