c2a5e3f0893454c08b26602d7ae33ea861dd1e73
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-05-31 23:55:35 +0530

    Dynamic styling using ngStyle directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'online';
 
+  constructor() {
+    this.serverStatus = Math.random() < 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
