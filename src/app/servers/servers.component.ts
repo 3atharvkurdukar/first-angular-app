acc7a516f33a5e5e9734425b151d9c61f189de88
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-05-31 13:35:17 +0530

    Using property binding to set atribute values

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
