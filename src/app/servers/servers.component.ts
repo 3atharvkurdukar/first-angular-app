e494115cf4fac798db2eec1e37eebd6c6eeaa5fc
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-05-31 12:42:06 +0530

    Using different selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers',   // Tag Selector
+  // selector: '.app-servers',  // Class Selector
+  // selector: '[app-servers]',    // Attribute Selector
   // template: '<app-server></app-server><app-server></app-server>'
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
